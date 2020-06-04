5af6dd725e8919d62dc0acaae8b42f63b8ef97af
Mufti Taha Shah <muftitahashah@gmail.com>
2020-06-04 11:47:41 +0530

    NA values

--- Rajya Sabha Members Cleaned Data Set.R
+++ Rajya Sabha Members Cleaned Data Set.R
@@ -5,3 +5,5 @@ Members <- read.csv("rs_member_terms.csv") # defining the data frame
 Members <- separate(Members, vacate_reason, c("vacate_date", "vacate_reason"), "\n", TRUE, TRUE) #to separate the vacate reason into date & reason 
 Members <- separate(Members,term, c("term_start", "term_end"), " ", TRUE, TRUE) #to separate the term column
 Members <- mutate(Members,vacate_reason = ifelse(is.na(vacate_reason), vacate_date, vacate_reason ) )
+filter(Members, is.na(Members$term_end))
+filter(Members, s_no==153)
