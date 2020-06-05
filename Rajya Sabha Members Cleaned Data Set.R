042b9e6e78b530f9d2986704ab44afede2682ef5
Mufti Taha Shah <muftitahashah@gmail.com>
2020-06-05 15:18:37 +0530

    Adding Normalized Parties File and Cleaning Vacate reasons/date, states and terms

--- Rajya Sabha Members Cleaned Data Set.R
+++ Rajya Sabha Members Cleaned Data Set.R
@@ -7,3 +7,16 @@ Members <- separate(Members,term, c("term_start", "term_end"), " ", TRUE, TRUE) 
 Members <- mutate(Members,vacate_reason = ifelse(is.na(vacate_reason), vacate_date, vacate_reason ) )
 filter(Members, is.na(Members$term_end))
 filter(Members, s_no==153)
+uniquestates<- unique(Members$state)
+uniqueParties<-unique(Members$party)
+print(unique(Members$vacate_reason))
+filter(Members, is.na(Members$vacate_date))
+filter(Members, is.na(Members$term_start))
+filter(Members, is.na(Members$term_end))
+
+
+setwd("C:/Users/Mufti Taha Shah/Documents/GitHub/Rajya-Sabha")
+read.csv("all_normalized_party_names.csv")
+NormalizedParties<- read.csv("all_normalized_party_names.csv")
+PartiesL<- unique(NormalizedParties$Expanded.Party.Name)
+
