967ae45e48d2cbc04e1b7b7de03f765ff73a3015
Mufti Taha Shah <muftitahashah@gmail.com>
2020-06-12 12:31:57 +0530

    Mapping Parties in the 60's

--- Rajya Sabha Members Cleaned Data Set.R
+++ Rajya Sabha Members Cleaned Data Set.R
@@ -59,7 +59,7 @@ NormalizedParties7 = NormalizedParties[NormalizedParties$Year>=1970 & Normalized
 NormalizedParties7 = unique(NormalizedParties7$Expanded.Party.Name)
 
 
-Members8[party=='CONG(I)',]$party='INDIAN NATIOANL CONGRESS (I)'
+Members8[party=='CONG(I)',]$party='INDIAN NATIONAL CONGRESS (I)'
 Members8[party=='BJP',]$party='BHARATIYA JANTA PARTY'
 Members8[party=='JD',]$party='JANATA DAL'
 Members8[party=='AIADMK',]$party='ALL INDIA ANNA DRAVIDA MUNNETRA KAZHAGAM'
@@ -78,3 +78,37 @@ Members8[party=='KC',]$party='KERALA CONGRESS'
 Members8[party=='J&KNC',]$party='JAMMU & KASHMIR NATIONAL CONFERENCE'
 Members8[party=='SAD',]$party='SHIROMANI AKALI DAL'
 #Coudn't do - AD,FB,CONG (S),RSP,AGP,TDP,O,JAN,JD(S),JD(U),SSP
+#https://rajyasabha.nic.in/rsnew/member_site/Main.aspx Mistry Smt. Roda	- Party 'Other'
+#https://rajyasabha.nic.in/rsnew/member_site/Main.aspx
+#Pandit Ravi Shankar State -Nominated My Political Party:	Other
+#Masood Shri Rasheed	: State from which I am elected:	Uttar Pradesh, My Political Party:	Indian National Congress
+#Naik Shri R.S.	:State from which I am elected:	Karnataka, My Political Party:	Janata
+Members8[party=='AD',]$party='AKALI DAL'
+Members8[party=='FB',]$party='FORWARD BLOCK'
+Members8[party=='RSP',]$party='REVOLUTIONARY SOCIALIST PARTY'
+Members8[party=='AGP',]$party='Asom Gana Parishad'
+Members8[party=='TDP',]$party='Telugu Desam Party'
+Members8[party=='CONG(I)',]$party='INDIAN NATIONAL CONGRESS (I)'
+Members8[member_name=='Lather Shri Mohinder Singh',]$party='JANATA DAL'
+Members8[member_name=='Matto Shri Ghulam Rasool',]$party='JAMMU & KASHMIR NATIONAL CONFERENCE'
+Members8[party=='JD(S)',]$party='Janata Dal (Secular)'
+Members8[party=='SSP',]$party='SIKKIMSAGRAM PARISHAD'
+
+#Coudn't do - CONG (O)
+#Basavapunnaiah Shri Makkineni- State from which I am elected:	Madras, My Political Party:	Communist
+Members6[party=='ML',]$party='MUSLIM LEAGUE'
+Members6[party=='Congress',]$party='INDIAN NATIONAL CONGRESS'
+Members6[party=='CPI',]$party='COMMUNIST PARTY OF INDIA'
+Members6[party=='NOM.',]$party='NOMINATED'
+Members6[party=='DMK',]$party='DRAVIDA MUNITRA KAZHAGAM'
+Members6[party=='PSP',]$party='PRAJA SOCIALIST PARTY'
+Members6[party=='Swatantra',]$party='SWATANTRA PARTY'
+Members6[party=='IND.',]$party='INDEPENDENT'
+Members6[party=='SSP',]$party='SAMYUKTA SOCIALIST PARTY'
+Members6[party=='FB',]$party='FORWARD BLOCK'
+Members6[party=='JS',]$party='JAN SANGH'
+Members6[party=='AD',]$party='AKALI DAL'
+Members6[party=='CPI(M)',]$party='COMMUNIST PARTY OF INDIA (MARXIST)'
+Members6[party=='BKD',]$party='BHARATIYA KRANTI DAL'
+Members6[party=='INC',]$party='INDIAN NATIONAL CONGRESS'
+Members6[party=='RPI',]$party='REPUBLICAN PARTY OF INDIA'
