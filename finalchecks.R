acb4d58dc8f16493cd356e94050c41c10a148443
Mufti Taha Shah <muftitahashah@gmail.com>
2020-07-31 15:51:16 +0530

    Update finalchecks.R

--- finalchecks.R
+++ finalchecks.R
@@ -3,3 +3,6 @@ termends <- sort(allmembers$term_end)
 vacation_date <- sort(allmembers$vacation_date)
 missingdates<- allmembers[is.na(allmembers$vacation_date),]
 missingreasons<- allmembers[allmembers$vacation_reason == '',]
+
+empty<- subset(Members7, party_normalized == '')
+print(unique(empty$party))
