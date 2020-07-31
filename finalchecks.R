f1dcc33f5e0328b79594836ee8ac6beefcb0c786
Mufti Taha Shah <muftitahashah@gmail.com>
2020-07-31 05:41:04 +0530

    Create finalchecks.R

---
+++ finalchecks.R
@@ -0,0 +1,5 @@
+termstarts<- sort(allmembers$term_start)
+termends <- sort(allmembers$term_end)
+vacation_date <- sort(allmembers$vacation_date)
+missingdates<- allmembers[is.na(allmembers$vacation_date),]
+missingreasons<- allmembers[allmembers$vacation_reason == '',]
